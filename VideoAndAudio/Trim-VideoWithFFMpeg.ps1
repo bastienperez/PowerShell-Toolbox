c647c56e62bf33f041e1fd751637326bce13fc1c
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-04-24 16:20:55 +0200

    fix: Correct string quotes in Trim-VideoWithFFMpeg.ps1 for consistency

--- VideoAndAudio/Trim-VideoWithFFMpeg.ps1
+++ VideoAndAudio/Trim-VideoWithFFMpeg.ps1
@@ -21,12 +21,12 @@ function Get-TimeInHHMMSSFormat {
         1 { return [TimeSpan]::FromSeconds([int]$timeParts[0]) }
         2 { return [TimeSpan]::FromMinutes([int]$timeParts[0]).Add([TimeSpan]::FromSeconds([int]$timeParts[1])) }
         3 { return [TimeSpan]::FromHours([int]$timeParts[0]).Add([TimeSpan]::FromMinutes([int]$timeParts[1])).Add([TimeSpan]::FromSeconds([int]$timeParts[2])) }
-        default { throw "Invalid time format. Please use one of the following formats: SS, MM:SS, or HH:MM:SS." }
+        default { throw 'Invalid time format. Please use one of the following formats: SS, MM:SS, or HH:MM:SS.' }
     }
 }
 
 # Get video duration using FFmpeg
-$duration = & ffmpeg -i $InputVideo 2>&1 | Select-String -Pattern "Duration: (\d+):(\d+):(\d+\.\d+)" | ForEach-Object { $_.Matches.Groups[1].Value, $_.Matches.Groups[2].Value, $_.Matches.Groups[3].Value -join ":" }
+$duration = & ffmpeg -i $InputVideo 2>&1 | Select-String -Pattern 'Duration: (\d+):(\d+):(\d+\.\d+)' | ForEach-Object { $_.Matches.Groups[1].Value, $_.Matches.Groups[2].Value, $_.Matches.Groups[3].Value -join ':' }
 
 # Use the custom Parse-Time function
 $startCutTimeSpan = Get-TimeInHHMMSSFormat -Time $StartCut
@@ -43,7 +43,7 @@ $endCutTimeSpan = Get-TimeInHHMMSSFormat -Time $EndCut
 $newDuration = $endCutTimeSpan.Subtract($startCutTimeSpan)
 
 # Format new duration for FFmpeg
-$newDuration = "{0:00}:{1:00}:{2:00}.{3:000}" -f $newDuration.Hours, $newDuration.Minutes, $newDuration.Seconds, $newDuration.Milliseconds
+$newDuration = '{0:00}:{1:00}:{2:00}.{3:000}' -f $newDuration.Hours, $newDuration.Minutes, $newDuration.Seconds, $newDuration.Milliseconds
 
 # Use FFmpeg to cut the video
 & ffmpeg -ss $StartCut -i $InputVideo -t $newDuration -c copy $OutputVideo
